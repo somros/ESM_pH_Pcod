a66a7ab4cd9b86e4dec2a59754feacb3344306d8
Alberto Rovellini <alberto.rovellini@gmail.com>
2023-06-26 15:06:41 -0700

    updated areas

--- forAndre.R
+++ forAndre.R
@@ -1,6 +1,5 @@
 # Prepare pH data for Andre
-library(dplyr)
-library(tidyr)
+library(tidyverse)
 
 dat1000 <- read.csv('esm_ph.csv')
 dat300 <- read.csv('esm_ph_300.csv')
@@ -10,13 +9,21 @@ dat300 <- read.csv('esm_ph_300.csv')
 # get weighted average of 610, 620, and 630
 
 # start from 1000
+# areas m^2 of 0-1000 m shelf by NMFS area
+# 610 63986698621 
+# 620 69583703140
+# 630 105918077937
+# 640 37270389681
+# 650 43952466109
+
 dat1000 <- dat1000 %>%
   filter(NMFS_AREA %in% c(610,620,630) & esm == 'GFDL') %>%
   pivot_wider(values_from = ph_mean, names_from = NMFS_AREA) %>%
   rename(pH_610 = `610`, pH_620 = `620`, pH_630 = `630`) %>%
   mutate(pH_610_620_630 = (pH_610 * 63986698621 + 
                              pH_620 * 69583703140 +
-                             pH_630 * 98582220025) / (63986698621 + 69583703140 + 98582220025))
+                             pH_630 * 105918077937) / (63986698621 + 69583703140 + 105918077937))
+
 
 # view
 dat1000 %>%
@@ -29,13 +36,19 @@ dat1000 %>%
 write.csv(dat1000, 'gfdl_ph_610_620_630_1000m.csv', row.names = F)
 
 # 300 m
+# areas m^2 of 0-300 m shelf by NMFS area
+# 610 57225003746
+# 620 62597059226
+# 630 98582220025
+# 640 32560976631
+# 650 36726651409
 dat300 <- dat300 %>%
   filter(NMFS_AREA %in% c(610,620,630) & esm == 'GFDL') %>%
   pivot_wider(values_from = ph_mean, names_from = NMFS_AREA) %>%
   rename(pH_610 = `610`, pH_620 = `620`, pH_630 = `630`) %>%
-  mutate(pH_610_620_630 = (pH_610 * 63986698621 + 
-                             pH_620 * 69583703140 +
-                             pH_630 * 98582220025) / (63986698621 + 69583703140 + 98582220025))
+  mutate(pH_610_620_630 = (pH_610 * 57225003746 + 
+                             pH_620 * 62597059226 +
+                             pH_630 * 98582220025) / (57225003746 + 62597059226 + 98582220025))
 
 # view
 dat300 %>%
@@ -46,17 +59,3 @@ dat300 %>%
 
 # write out for Andre
 write.csv(dat300, 'gfdl_ph_610_620_630_300m.csv', row.names = F)
-
-# areas m^2 of 0-300 m shelf by NMFS area
-# 610 57225003746
-# 620 62597059226
-# 630 98582220025
-# 640 32560976631
-# 650 36726651409
-
-# areas m^2 of 0-1000 m shelf by NMFS area
-# 610 63986698621 
-# 620 69583703140
-# 630 105918077937
-# 640 37270389681
-# 650 43952466109
